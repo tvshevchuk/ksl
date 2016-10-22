71d0cb693f4e6e90d36e437fb887fc3afd36e10f
tshevchuk <tvshevchuk@gmail.com>
2016-10-22 16:51:47 +0300

    Close after click

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ for (var i = 0; i < playerBoxes.length; i++) {
 }
 
 
+modalContent.onclick = function() {
+    modal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
